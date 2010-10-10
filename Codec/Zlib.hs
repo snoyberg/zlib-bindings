a7609bd528c99e9a88e9d5d4688a98c3439b83ca
Michael Snoyman <michael@snoyman.com>
2010-10-10 05:45:35 +0200

    ZStream -> (Inflate, Deflate)

--- Codec/Zlib.hs
+++ Codec/Zlib.hs
@@ -2,7 +2,8 @@
 {-# LANGUAGE EmptyDataDecls #-}
 module Codec.Zlib
     ( -- * Data types
-      ZStream
+      Inflate
+    , Deflate
     , WindowBits (..)
       -- * Inflate
     , initInflate
@@ -27,7 +28,9 @@ import Control.Monad (when)
 
 data ZStreamStruct
 type ZStream' = Ptr ZStreamStruct
-type ZStream = (ForeignPtr ZStreamStruct, ForeignPtr CChar)
+type ZStreamPair = (ForeignPtr ZStreamStruct, ForeignPtr CChar)
+newtype Inflate = Inflate ZStreamPair
+newtype Deflate = Deflate ZStreamPair
 
 foreign import ccall unsafe "create_z_stream_inflate"
     c_create_z_stream_inflate :: CInt -> IO ZStream'
@@ -35,7 +38,7 @@ foreign import ccall unsafe "create_z_stream_inflate"
 foreign import ccall unsafe "&free_z_stream_inflate"
     c_free_z_stream_inflate :: FunPtr (ZStream' -> IO ())
 
-initInflate :: WindowBits -> IO ZStream
+initInflate :: WindowBits -> IO Inflate
 initInflate w = do
     let w' = case w of
                 DefaultWindowBits -> 15
@@ -45,7 +48,7 @@ initInflate w = do
     fbuff <- mallocForeignPtrBytes defaultChunkSize
     withForeignPtr fbuff $ \buff ->
         c_set_avail_out zstr buff $ fromIntegral defaultChunkSize
-    return (fzstr, fbuff)
+    return $ Inflate (fzstr, fbuff)
 
 foreign import ccall unsafe "create_z_stream_deflate"
     c_create_z_stream_deflate :: CInt -> IO ZStream'
@@ -53,7 +56,7 @@ foreign import ccall unsafe "create_z_stream_deflate"
 foreign import ccall unsafe "&free_z_stream_deflate"
     c_free_z_stream_deflate :: FunPtr (ZStream' -> IO ())
 
-initDeflate :: WindowBits -> IO ZStream
+initDeflate :: WindowBits -> IO Deflate
 initDeflate w = do
     let w' = case w of
                 DefaultWindowBits -> 15
@@ -63,7 +66,7 @@ initDeflate w = do
     fbuff <- mallocForeignPtrBytes defaultChunkSize
     withForeignPtr fbuff $ \buff ->
         c_set_avail_out zstr buff $ fromIntegral defaultChunkSize
-    return (fzstr, fbuff)
+    return $ Deflate (fzstr, fbuff)
 
 foreign import ccall unsafe "set_avail_in"
     c_set_avail_in :: ZStream' -> Ptr CChar -> CUInt -> IO ()
@@ -81,9 +84,9 @@ foreign import ccall unsafe "call_inflate_noflush"
     c_call_inflate_noflush :: ZStream' -> IO CInt
 
 withInflateInput
-    :: ZStream -> S.ByteString -> (IO (Maybe S.ByteString) -> IO a)
+    :: Inflate -> S.ByteString -> (IO (Maybe S.ByteString) -> IO a)
     -> IO a
-withInflateInput (fzstr, fbuff) bs f =
+withInflateInput (Inflate (fzstr, fbuff)) bs f =
     withForeignPtr fzstr $ \zstr ->
         unsafeUseAsCStringLen bs $ \(cstr, len) -> do
             c_set_avail_in zstr cstr $ fromIntegral len
@@ -108,8 +111,8 @@ drain fbuff zstr func isFinish = do
                     return $ Just bs
                 else return Nothing
 
-finishInflate :: ZStream -> IO S.ByteString
-finishInflate (fzstr, fbuff) =
+finishInflate :: Inflate -> IO S.ByteString
+finishInflate (Inflate (fzstr, fbuff)) =
     withForeignPtr fzstr $ \zstr ->
         withForeignPtr fbuff $ \buff -> do
             avail <- c_get_avail_out zstr
@@ -120,8 +123,8 @@ foreign import ccall unsafe "call_deflate_noflush"
     c_call_deflate_noflush :: ZStream' -> IO CInt
 
 withDeflateInput
-    :: ZStream -> S.ByteString -> (IO (Maybe S.ByteString) -> IO a) -> IO a
-withDeflateInput (fzstr, fbuff) bs f =
+    :: Deflate -> S.ByteString -> (IO (Maybe S.ByteString) -> IO a) -> IO a
+withDeflateInput (Deflate (fzstr, fbuff)) bs f =
     withForeignPtr fzstr $ \zstr ->
         unsafeUseAsCStringLen bs $ \(cstr, len) -> do
             c_set_avail_in zstr cstr $ fromIntegral len
@@ -130,7 +133,7 @@ withDeflateInput (fzstr, fbuff) bs f =
 foreign import ccall unsafe "call_deflate_finish"
     c_call_deflate_finish :: ZStream' -> IO CInt
 
-finishDeflate :: ZStream -> (IO (Maybe S.ByteString) -> IO a) -> IO a
-finishDeflate (fzstr, fbuff) f =
+finishDeflate :: Deflate -> (IO (Maybe S.ByteString) -> IO a) -> IO a
+finishDeflate (Deflate (fzstr, fbuff)) f =
     withForeignPtr fzstr $ \zstr ->
         f $ drain fbuff zstr c_call_deflate_finish True
